7b5c638d52e5f93e1f03f8ef50a77012b1b2813e
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-08-24 16:28:11 +0300

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -17,61 +17,7 @@ def test_api_responses(endpoint, expected_status):
     """
     with allure.step(f"Отправка GET запроса на {endpoint}"):
         response = requests.get(f"{BASE_URL_API}{endpoint}", headers=HEADERS)
-    
+
     with allure.step(f"Проверка, что статус ответа равен {expected_status}"):
         assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
 
-@allure.feature('Search by ID')
-def test_search_content_by_id():
-    """
-    Тест API на поиск контента по ID.
-    """
-    with allure.step("Отправка GET запроса на https://api.kinopoisk.dev/v1.4/movie/10000"):
-        response = requests.get(f"{BASE_URL_API}/movie/10000", headers=HEADERS)
-    
-    with allure.step("Проверка, что статус ответа равен 200"):
-        assert response.status_code == 200
-
-@allure.feature('Search by Title')
-def test_search_content_by_title():
-    """
-    Тест API на поиск контента по названию.
-    """
-    with allure.step("Отправка GET запроса на https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=Лобстер"):
-        response = requests.get(f"{BASE_URL_API}/movie/search?page=1&limit=10&query=Лобстер", headers=HEADERS)
-    
-    with allure.step("Проверка, что статус ответа равен 200"):
-        assert response.status_code == 200
-
-@allure.feature('Search Persons by Name')
-def test_search_person_by_name():
-    """
-    Тест API на поиск актеров, режиссеров по имени/фамилии.
-    """
-    with allure.step("Отправка GET запроса на https://api.kinopoisk.dev/v1.4/person/search?page=1&limit=10&query=Стэйтем"):
-        response = requests.get(f"{BASE_URL_API}/person/search?page=1&limit=10&query=Стэйтем", headers=HEADERS)
-    
-    with allure.step("Проверка, что статус ответа равен 200"):
-        assert response.status_code == 200
-
-@allure.feature('Search Content with Invalid Parameters')
-def test_search_content_invalid_params():
-    """
-    Тест API на поиск контента с неверными параметрами запроса.
-    """
-    with allure.step("Отправка GET запроса на https://api.kinopoisk.dev/v1.4/movie/search?page=0&limit=0&query=Лобстер"):
-        response = requests.get(f"{BASE_URL_API}/movie/search?page=0&limit=0&query=Лобстер", headers=HEADERS)
-    
-    with allure.step("Проверка, что статус ответа равен 400"):
-        assert response.status_code == 400
-
-@allure.feature('Search Content by ID with Out-of-Bounds Value')
-def test_search_content_id_out_of_bounds():
-    """
-    Тест API на поиск контента по ID, выходящему за допустимые значения.
-    """
-    with allure.step("Отправка GET запроса на https://api.kinopoisk.dev/v1.4/movie/1000000000000"):
-        response = requests.get(f"{BASE_URL_API}/movie/1000000000000", headers=HEADERS)
-    
-    with allure.step("Проверка, что статус ответа равен 400"):
-        assert response.status_code == 400

f3ccdc05dc48f028556520fd724e2af1490e6996
marutsan88 <156997392+marutsan88@users.noreply.github.com>
2024-08-24 08:16:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://api.kinopoisk.dev/v1.4"
+BASE_URL_API = "https://api.kinopoisk.dev/v1.4"
 HEADERS = {
     "accept": "application/json",
     "X-API-KEY": "FVNX1T1-6Q5MYB7-GZARQBN-W82KWVG"
